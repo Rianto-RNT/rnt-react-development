c7637177950cfa5963342740ee55e5acc03c7ebc
Rianto <rianto.rnt@gmail.com>
2022-05-22 22:32:58 +0800

    Refactor Homepage and Directory Component

---
+++ rnt-cloth/src/pages/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../assets/scss/homepage.style.scss";
+
+import Directory from "../../components/directory/directory";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
