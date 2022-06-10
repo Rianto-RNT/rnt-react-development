207cb33041be14744d986afe217ed1259968ac5a
Rianto <rianto.rnt@gmail.com>
2022-06-10 11:28:36 +0800

    switch style of custom button to scss style

--- rnt-clothing/src/components/custom-button/custom-button.jsx
+++ rnt-clothing/src/components/custom-button/custom-button.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
-import { CustomButtonContainer } from "./custom-button.styles";
+// import { CustomButtonContainer } from "./custom-button.styles";
 
-const CustomButton = ({ children, ...props }) => (
-  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+import '../../assets/scss/custom-button.style.scss'
+
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
 );
+
+// // Using Styled-component library
+// const CustomButton = ({ children, ...props }) => (
+//   <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+// );
 
 export default CustomButton;
