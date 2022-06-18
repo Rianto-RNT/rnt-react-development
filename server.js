6f0a3884faaba730161896319c430e5e9039ac21
Rianto <rianto.rnt@gmail.com>
2022-06-18 13:33:43 +0800

    Connecting client to server

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { send } = require('process');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -17,10 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if ((process.env.NODE_ENV = 'production')) {
-  app.use(express.static(path.join(__dirname, 'client/rnt-clothing/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/rnt-clothing', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'index.js'));
   });
 }
 
