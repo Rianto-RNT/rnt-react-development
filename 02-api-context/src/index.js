3de458fab9efd4e2c1c70912d2f1edb717732b3d
Rianto <rianto.rnt@gmail.com>
2022-06-20 10:52:34 +0800

    Context consumer + useContext Hook

--- 02-api-context/src/index.js
+++ 02-api-context/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -7,15 +7,19 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 
 import './index.css';
-import App from './App';
 
-ReactDOM.render(
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
+
+reportWebVitals();
