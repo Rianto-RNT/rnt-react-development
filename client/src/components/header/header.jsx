42f332700c9713af53acdf85dbce5eb6859c61a6
Rianto <rianto.rnt@gmail.com>
2022-07-10 19:40:40 +0800

    Bugs fix - Component #2

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import CartIcon from "../cart-icon/cart-icon";
-import CartDropdown from "../cart-dropdown/cart-dropdown";
-import { selectCartHidden } from "../../redux/cart/cart.selectors";
-import { selectCurrentUser } from "../../redux/user/user.selector";
-import { signOutStart } from "../../redux/user/user.actions";
+import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selector';
+import { signOutStart } from '../../redux/user/user.actions';
 
-import { ReactComponent as Logo } from "../../assets/images/svg/crown.svg";
+import { ReactComponent as Logo } from '../../assets/images/svg/crown.svg';
 
 import {
   HeaderContainer,
   LogoContainer,
   OptionsContainer,
   OptionLink,
-} from "./header.styles";
+} from './header.styles';
 
-const Header = ({ currentUser, hidden, signOutStart }) => (
+export const Header = ({ currentUser, hidden, signOutStart }) => (
   <HeaderContainer>
     <LogoContainer to="/">
       <Logo className="logo" />
